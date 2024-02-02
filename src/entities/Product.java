902b096d4aadb52abc3de826dcc8c70e5934f972
4Miguel2 <delgdevoleper@gmail.com>
2024-02-02 16:51:17 -0300

    Reference method com metodo nao estatico

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -24,11 +24,16 @@ public class Product{
     public void setPrice(Double price) {
         this.price = price;
     }
-
+    //Passamos o argumento pois é um método static
+    //Métodos estáticos pertencem à classe e não têm acesso direto a instâncias da classe
+    //Eles são associados à classe como um todo, não a instâncias específicas.
     public static boolean staticProductPredicate(Product p) {
         return p.getPrice() >= 100.0;
     }
-
+    //Não passamos o objeto como argumento pois já estamos dentro da instância da classe.
+    public boolean nonStaticProductPredicate() {
+        return price >= 100.0;
+    }
     @Override
     public String toString() {
         return name+", "+ String.format("%.2f", price);
