7c735780ffd6fbd3e172f0b401bb146d6c1f404d
4Miguel2 <delgdevoleper@gmail.com>
2024-02-03 21:40:50 -0300

    organizando pacotes

--- src/compareTo_and_Predicate/compareTo01.java
+++ src/compareTo_and_Predicate/compareTo01.java
@@ -5,7 +5,7 @@ import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Demo02 {
+public class compareTo {
     public static int compareProducts(Product p1, Product p2) {
         return p1.getPrice().compareTo(p2.getPrice());
     }
@@ -18,7 +18,7 @@ public class Demo02 {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        list.sort(Demo02::compareProducts);
+        list.sort(compareTo::compareProducts);
 
         list.forEach(System.out::println);
     }
